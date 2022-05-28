b1bcc4a40ca65db3f608d95dfa695bc5a7b97f2f
Adu Sarah <adusarah@Adus-Mackbook.local>
2022-05-28 12:48:17 +0100

    Anagram

---
+++ main.py
@@ -0,0 +1,16 @@
+# Check if two words are anagrams 
+# Example:
+# find_anagrams("hello", "check") --> False
+# find_anagrams("below", "elbow") --> True
+
+
+def find_anagram(word, anagram):
+    # [assignment] Add your code here
+    find_anagrams("hello", "check")
+    find_anagrams("below", "elbow")
+    if (sorted(word)==sorted(anagram)):
+        print("They are anagrams")
+    return True
+ else:
+        print("They are not anagrams ")
+        return False
